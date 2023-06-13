add41e792ce7378098aabe4f7ec070414413aa82
mizok <mizok.huang@neutec.com.tw>
2023-06-13 18:46:49 +0800

    把各種test都切成分支

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NeuTranslocoDirective } from './neu-transloco.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NeuTranslocoDirective,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   exports:[
-    NeuTranslocoDirective,
   ],
   providers: [],
   bootstrap: [AppComponent]
