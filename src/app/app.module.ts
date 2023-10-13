4c116f1193e9c8fed587c77d7bbabdb2cc0b7eab
mizok <mizok.huang@neutec.com.tw>
2023-10-13 18:36:03 +0800

    移除測試內容

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import MyComponent from './builder-comp/builder-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent
   ],
   imports: [
     BrowserModule,
