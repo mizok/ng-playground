a1ca1d5e875adffe7db07c5534909a0a53fa668e
mizok <mizok.huang@neutec.com.tw>
2023-10-13 14:51:41 +0800

    test builder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import MyComponent from './builder-comp/builder-comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule,
