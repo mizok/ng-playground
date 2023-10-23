1e97b7959d25ee63a3ac29a61ff9ed7e08ecb61f
mizok <mizok.huang@neutec.com.tw>
2023-10-23 12:12:59 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-playground';
+  private subject$ = new ReplaySubject(1);
   constructor(){
   }
+
+  ngOnInit(): void {
+      this.subject$.next(1);
+      this.subject$.subscribe((o)=>{console.log(o)});
+  }
+
 
 }
