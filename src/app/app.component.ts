b8681ab3cbb6ab11dc4cd20b1d41500a9018c52f
mizok <mizok.huang@neutec.com.tw>
2023-10-23 12:03:31 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, tap, zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-playground';
+export class AppComponent implements OnInit{
+  private subject1$ = new Subject<void>();
+  private subject2$ = new Subject<void>();
+  private subject3$ = new Subject<void>();
+
   constructor(){
   }
+
+  ngOnInit(): void {
+    zip(this.subject1$,this.subject2$,this.subject3$).pipe(
+    ).subscribe();
+
+
+    setTimeout(()=>{
+      this.subject1$.next();
+    },1000);
+    setTimeout(()=>{
+      this.subject2$.next();
+    },2000);
+    setTimeout(()=>{
+      this.subject3$.next();
+    },3000);
+
+  }
+
 
 }
