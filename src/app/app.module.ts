e7fb0d170880bbe1a9fa440d06c3195da8792c88
mizok <mizok.huang@neutec.com.tw>
2023-10-30 11:02:18 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TestComponent } from './test/test.component'
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TestComponent
   ],
   exports:[
   ],
